dc55c2da1c55b36e6726e3d07e10de8b01c7503b
DonMB77 <purposefuldrifter@gmail.com>
2025-01-23 22:08:04 +0100

    Create/ Update date through auditing

--- src/test/java/com/drifter/spring_6_reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/com/drifter/spring_6_reactive/repositories/BeerRepositoryTest.java
@@ -1,15 +1,18 @@
 package com.drifter.spring_6_reactive.repositories;
 
+import com.drifter.spring_6_reactive.config.DatabaseConfig;
 import com.drifter.spring_6_reactive.domain.Beer;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
+@Import(DatabaseConfig.class)
 class BeerRepositoryTest {
 
     @Autowired
