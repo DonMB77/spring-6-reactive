0b8f56060e1796ddb66a36775b055c58362f385d
DonMB77 <purposefuldrifter@gmail.com>
2025-01-23 22:04:44 +0100

    BeerRepositoryTest + Lombok version

--- src/main/java/com/drifter/spring_6_reactive/repositories/BeerRepository.java
+++ src/main/java/com/drifter/spring_6_reactive/repositories/BeerRepository.java
@@ -4,5 +4,5 @@ import com.drifter.spring_6_reactive.domain.Beer;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
 
 public interface BeerRepository extends ReactiveCrudRepository<Beer, Integer> {
-    
+
 }
