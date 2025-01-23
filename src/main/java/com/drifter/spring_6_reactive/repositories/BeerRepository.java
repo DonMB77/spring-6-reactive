2ef188524fef7225262af83a986e8921d7d19266
DonMB77 <purposefuldrifter@gmail.com>
2025-01-23 21:58:30 +0100

    Create BeerRepository.java

---
+++ src/main/java/com/drifter/spring_6_reactive/repositories/BeerRepository.java
@@ -0,0 +1,8 @@
+package com.drifter.spring_6_reactive.repositories;
+
+import com.drifter.spring_6_reactive.domain.Beer;
+import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+
+public interface BeerRepository extends ReactiveCrudRepository<Beer, Integer> {
+    
+}
