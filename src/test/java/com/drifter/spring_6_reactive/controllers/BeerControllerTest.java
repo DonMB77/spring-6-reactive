417d7797b88ab4806db08235f6c0384dfe92c60f
DonMB77 <purposefuldrifter@gmail.com>
2025-01-24 20:36:28 +0100

    BeerControllerTest.testCreateBeer()

--- src/test/java/com/drifter/spring_6_reactive/controllers/BeerControllerTest.java
+++ src/test/java/com/drifter/spring_6_reactive/controllers/BeerControllerTest.java
@@ -1,11 +1,14 @@
 package com.drifter.spring_6_reactive.controllers;
 
 import com.drifter.spring_6_reactive.domain.BeerDTO;
+import com.drifter.spring_6_reactive.repositories.BeerRepositoryTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.web.reactive.server.WebTestClient;
+import reactor.core.publisher.Mono;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -15,6 +18,17 @@ class BeerControllerTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    void testCreateBeer() {
+
+        webTestClient.post().uri(BeerController.LOCALHOST_BEER_PATH)
+                .body(Mono.just(BeerRepositoryTest.getTestBeer()), BeerDTO.class)
+                .header("Content-type", "application/json")
+                .exchange()
+                .expectStatus().isCreated()
+                .expectHeader().location("http://localhost:8080/api/v2/beer/4");
+    }
 
     @Test
     void testBeerById() {
--- src/test/java/com/drifter/spring_6_reactive/repositories/BeerRepositoryTest.java
+++ src/test/java/com/drifter/spring_6_reactive/repositories/BeerRepositoryTest.java
@@ -15,7 +15,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 @DataR2dbcTest
 @Import(DatabaseConfig.class)
-class BeerRepositoryTest {
+public class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
@@ -34,7 +34,7 @@ class BeerRepositoryTest {
                 });
     }
 
-    Beer getTestBeer() {
+    public static Beer getTestBeer() {
         return Beer.builder()
                 .beerName("Space Dust")
                 .beerStyle("IPA")
