a468d32b3f2d7bda8b912fc270e4920c11c8ca53
DonMB77 <purposefuldrifter@gmail.com>
2025-01-24 20:23:42 +0100

    BeerControllerTest.testBeerById()

--- src/test/java/com/drifter/spring_6_reactive/controllers/BeerControllerTest.java
+++ src/test/java/com/drifter/spring_6_reactive/controllers/BeerControllerTest.java
@@ -1,5 +1,6 @@
 package com.drifter.spring_6_reactive.controllers;
 
+import com.drifter.spring_6_reactive.domain.BeerDTO;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
@@ -14,6 +15,15 @@ class BeerControllerTest {
 
     @Autowired
     WebTestClient webTestClient;
+
+    @Test
+    void testBeerById() {
+        webTestClient.get().uri(BeerController.LOCALHOST_BEER_PATH_WITH_ID, 1)
+                .exchange()
+                .expectStatus().isOk()
+                .expectHeader().valueEquals("Content-type", "application/json")
+                .expectBody(BeerDTO.class);
+    }
 
     @Test
     void testListBeers() {
