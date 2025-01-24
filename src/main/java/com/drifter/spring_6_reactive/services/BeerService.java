d8d5c4b1fedf8e15ae9bb7c411eb7842a6b46b1c
DonMB77 <purposefuldrifter@gmail.com>
2025-01-24 14:54:34 +0100

    BeerService.BeerService() impl

--- src/main/java/com/drifter/spring_6_reactive/services/BeerService.java
+++ src/main/java/com/drifter/spring_6_reactive/services/BeerService.java
@@ -3,8 +3,11 @@ package com.drifter.spring_6_reactive.services;
 import com.drifter.spring_6_reactive.domain.BeerDTO;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 public interface BeerService {
+
+    Mono<BeerDTO> getBeerById(Integer beerId);
 
     Flux<BeerDTO> listBeers();
 }
--- src/main/java/com/drifter/spring_6_reactive/services/BeerServiceImpl.java
+++ src/main/java/com/drifter/spring_6_reactive/services/BeerServiceImpl.java
@@ -1,11 +1,13 @@
 package com.drifter.spring_6_reactive.services;
 
+import com.drifter.spring_6_reactive.domain.Beer;
 import com.drifter.spring_6_reactive.domain.BeerDTO;
 import com.drifter.spring_6_reactive.mappers.BeerMapper;
 import com.drifter.spring_6_reactive.repositories.BeerRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
 @RequiredArgsConstructor
@@ -13,6 +15,11 @@ public class BeerServiceImpl implements BeerService {
 
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
+
+    @Override
+    public Mono<BeerDTO> getBeerById(Integer beerId) {
+        return beerRepository.findById(beerId).map(beerMapper::beerToBeerDto);
+    }
 
     @Override
     public Flux<BeerDTO> listBeers() {
